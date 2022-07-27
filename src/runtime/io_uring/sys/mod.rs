9bbb2f5998d13e666ab89922084ec2f29335e951
Dennis Krasnov <dennis@krasnov.dev>
2022-07-26 22:20:07 -0400

    remove direct syscall from runtime/io_uring/sys

--- src/runtime/io_uring/sys/mod.rs
+++ src/runtime/io_uring/sys/mod.rs
@@ -15,7 +15,6 @@ const SYSCALL_REGISTER: c_long = libc::SYS_io_uring_register;
 const SYSCALL_SETUP: c_long = libc::SYS_io_uring_setup;
 const SYSCALL_ENTER: c_long = libc::SYS_io_uring_enter;
 
-#[cfg(not(feature = "direct-syscall"))]
 pub unsafe fn io_uring_register(
     fd: c_int,
     opcode: c_uint,
@@ -31,33 +30,10 @@ pub unsafe fn io_uring_register(
     ) as _
 }
 
-#[cfg(feature = "direct-syscall")]
-pub unsafe fn io_uring_register(
-    fd: c_int,
-    opcode: c_uint,
-    arg: *const c_void,
-    nr_args: c_uint,
-) -> c_int {
-    sc::syscall4(
-        SYSCALL_REGISTER as usize,
-        fd as usize,
-        opcode as usize,
-        arg as usize,
-        nr_args as usize,
-    ) as _
-}
-
-#[cfg(not(feature = "direct-syscall"))]
 pub unsafe fn io_uring_setup(entries: c_uint, p: *mut io_uring_params) -> c_int {
     syscall(SYSCALL_SETUP, entries as c_long, p as c_long) as _
 }
 
-#[cfg(feature = "direct-syscall")]
-pub unsafe fn io_uring_setup(entries: c_uint, p: *mut io_uring_params) -> c_int {
-    sc::syscall2(SYSCALL_SETUP as usize, entries as usize, p as usize) as _
-}
-
-#[cfg(not(feature = "direct-syscall"))]
 pub unsafe fn io_uring_enter(
     fd: c_int,
     to_submit: c_uint,
@@ -74,25 +50,5 @@ pub unsafe fn io_uring_enter(
         flags as c_long,
         arg as c_long,
         size as c_long,
-    ) as _
-}
-
-#[cfg(feature = "direct-syscall")]
-pub unsafe fn io_uring_enter(
-    fd: c_int,
-    to_submit: c_uint,
-    min_complete: c_uint,
-    flags: c_uint,
-    arg: *const libc::c_void,
-    size: usize,
-) -> c_int {
-    sc::syscall6(
-        SYSCALL_ENTER as usize,
-        fd as usize,
-        to_submit as usize,
-        min_complete as usize,
-        flags as usize,
-        arg as usize,
-        size,
     ) as _
 }
