fa0ae56a5243cb7575e69e948bd130b6a77bdafc
Dennis Krasnov <dennis@krasnov.dev>
2023-08-24 23:55:41 -0400

    add start macro

---
+++ macros/src/lib.rs
@@ -0,0 +1,23 @@
+use proc_macro::TokenStream;
+
+use quote::quote;
+use syn::{parse_macro_input, ItemFn};
+
+#[proc_macro_attribute]
+pub fn start(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let item = parse_macro_input!(item as ItemFn);
+
+    let attributes = &item.attrs;
+    let visibility = &item.vis;
+    let signature = &item.sig;
+    let body = &item.block;
+
+    let result = quote! {
+        #(#attributes)*
+        #visibility #signature {
+            ::uringy::runtime::start(move || #body)
+        }
+    };
+
+    result.into()
+}
